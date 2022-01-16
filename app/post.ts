337d87cd1115c229ffea9b028efea8803afd7444
Atreya <44151328+atreya2011@users.noreply.github.com>
2022-01-16 15:06:57 +0900

    feat: pull post list from local fs

--- app/post.ts
+++ app/post.ts
@@ -1,17 +1,35 @@
+import parseFrontMatter from "front-matter";
+import fs from "fs/promises";
+import path from "path";
+import invariant from "tiny-invariant";
+
 export type Post = {
   slug: string;
   title: string;
 };
 
-export function getPosts(): Post[] {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mix-tape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+export type PostMarkdownAttributes = {
+  title: string;
 };
+
+const postsPath = path.join(__dirname, "..", "posts");
+
+function isValidPostAttributes(attributes: any): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
+
+export async function getPosts(): Promise<Post[]> {
+  const files = await fs.readdir(postsPath);
+  return Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(postsPath, file);
+      const data = await fs.readFile(filePath);
+      const { attributes } = parseFrontMatter(data.toString());
+      invariant(isValidPostAttributes(attributes), `${file} has bad meta data!`);
+      return {
+        slug: file.replace(/\.md$/, ""),
+        title: attributes.title,
+      };
+    })
+  );
+}
